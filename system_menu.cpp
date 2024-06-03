e26cb537d12e093c89452029a9e408f91c3519c0
Junyeok Seo <jun2342@chungbuk.ac.kr>
2024-06-04 00:14:08 +0900

    ShowMenu 함수 작성

--- system_menu.cpp
+++ system_menu.cpp
@@ -1,8 +1,42 @@
 #include "system_menu.h"
 
+void Display::SetCursorPosition(int x, int y)
+{
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
 void Display::ShowMenu()
 {
+	CleanDisplay();
+	SetCursorPosition(0, 0);
+	std::cout << "------------Menu-------------" << std::endl;
+	std::cout << "1. 고객관리" << std::endl;
+	std::cout << "2. 마일리지 관리" << std::endl;
+	std::cout << "3. 통계" << std::endl;
 
+	SetCursorPosition(0, 5);
+	std::cout << "---------오늘의 일정----------" << std::endl;
+	std::cout << "┌";
+	for (int i = 0; i < box_x; i++)
+		std::cout << "─";
+	std::cout << "┐";
+
+	for (int i = 7; i < box_y + 10; i++)
+	{
+		SetCursorPosition(0, i);
+		std::cout << "│";
+		SetCursorPosition(box_x + 1, i);
+		std::cout << "│";
+	}
+
+	SetCursorPosition(0, box_y + 9);
+	std::cout << "└";
+	for (int i = 0; i < box_x; i++)
+		std::cout << "─";
+	std::cout << "┘";
 }
 
 void Display::ShowMemMenu()
--- system_menu.h
+++ system_menu.h
@@ -1,15 +1,22 @@
 #ifndef SYSTEM_MENU_H
 #define SYSTEM_MENU_H
+#include <iostream>
+#include <windows.h>
 
 class Display
 {
 private:
 	int center;
+	int box_x = 30;
+	int box_y = 10;
 public:
+	void SetCursorPosition(int x, int y);
 	void ShowMenu();
 	void ShowMemMenu();
 	void ShowDsnMenu();
 	void Register();
+
+	void CleanDisplay() { system("cls"); }
 };
 
 #endif
